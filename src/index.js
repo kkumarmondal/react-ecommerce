b59faeddc9cee038be56b26f964462b3f7a619a3
kirankm <kirankm@cnetric.com>
2018-05-07 12:14:57 +0530

    remove unwanted file for bc

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import promiseMiddleware from 'redux-promise';
 import Router from './router'
 
 import reducers from './reducers'
-import './loader'
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
 
